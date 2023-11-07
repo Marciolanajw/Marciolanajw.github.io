030c955bf9c5e27a3acebc002609004d15a58101
Marciolanajw <140751478+Marciolanajw@users.noreply.github.com>
2023-11-07 13:59:43 -0300

    Update script_mes.js

--- script_mes.js
+++ script_mes.js
@@ -1,13 +1,1 @@
-var loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
 
-                    if (loggedInUser) {
-                        document.getElementById("profileName").textContent = loggedInUser.nome;
-                        document.getElementById("Publicador").value = loggedInUser.nome;
-                        var monthName = new Date().toLocaleString('default', { month: 'long' }); // Obtém o nome completo do mês
-                        var capitalizedMonth = monthName.charAt(0).toUpperCase() + monthName.slice(1); // Capitaliza a primeira letra do mês
-                        document.getElementById("currentMonth").textContent = capitalizedMonth;
-                        document.getElementById("profileImage").src = "fotos/" + loggedInUser.imagem;
-                    } else {
-                        alert("Nenhum usuário logado. Faça login primeiro.");
-                        window.location.href = "login.html";
-                    }
